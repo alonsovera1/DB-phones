6ffe2bc11ffbfb3b03ef93333ad5c0934e31188f
Alonso <alonsovera0101@gmail.com>
2023-04-18 08:56:57 -0430

    Realizar conexion

---
+++ controller.py
@@ -0,0 +1,46 @@
+#Insertar registro en una tabla
+import sqlite3
+
+#Realizar conexion
+def crear_conexion(base_datos):
+    try:
+        conexion = sqlite3.connect(base_datos)
+
+        return conexion
+    except sqlite3.Error as error:
+        print('Se ha producido un error al crear la conexion:',error)
+
+def crear_tabla(conexion, definicion):
+	cursor = conexion.cursor()
+	cursor.execute(definicion)
+	conexion.commit()
+
+def mostrar_tablas(conexion):
+	sql = "SELECT name FROM sqlite_master WHERE type='table';"
+
+	cursor = conexion.cursor()
+	cursor.execute(sql)
+	tablas = cursor.fetchall()
+
+	for t in tablas:
+		print(t[0])
+
+def crear_usuario(conexion, usuario):
+	sql = 'INSERT INTO usuario VALUES (?, ?, ?);'
+
+	cursor = conexion.cursor()
+	cursor.execute(sql, usuario)
+
+	conexion.commit()
+
+conexion = crear_conexion('registro.db')
+
+#Admision de tablas
+sql = """
+CREATE TABLE usuario (
+	id INTEGER NOT NULL,
+	nombre TEXT NOT NULL,
+	clave TEXT NOT NULL
+);
+"""
+crear_tabla(conexion, sql)
